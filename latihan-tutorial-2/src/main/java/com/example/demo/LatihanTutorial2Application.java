ede5890ff3230646da5d518677edbba23beaf35a
Muhammad Rifqi Fathurrahman <mrifqifathurrahman@gmail.com>
2018-09-19 20:45:28 +0700

    tutorial 2 APAP

---
+++ latihan-tutorial-2/src/main/java/com/example/demo/LatihanTutorial2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatihanTutorial2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatihanTutorial2Application.class, args);
+	}
+}
